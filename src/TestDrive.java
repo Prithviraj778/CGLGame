002b049041d203c10b6621ac05e7c60ac5aae938
G.Prithvi raj <71496259+Prithviraj778@users.noreply.github.com>
2020-12-22 21:12:26 +0530

    Prithvi Start

---
+++ src/TestDrive.java
@@ -0,0 +1,16 @@
+
+public class TestDrive 
+{
+
+	public TestDrive() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author G.Prithvi raj
+ *
+ */
+module Gameisnotfun {
+}
